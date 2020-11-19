3c8e013e71b775c958118a0fae86f8fb3fa53c1f
Arun Kumar <arunkumar2021@u.northwestern.edu>
2020-11-19 16:42:42 -0600

    commented fuse_pc node

--- camera_reconstruct/nodes/fuse_pc.cpp
+++ camera_reconstruct/nodes/fuse_pc.cpp
@@ -1,9 +1,13 @@
 #include "ros/ros.h"
 #include "sensor_msgs/PointCloud2.h"
-// #include "std_msgs/String.h"
 #include "pcl_conversions/pcl_conversions.h"
 
 class FusePC
+/*
+gets saved point clouds, fuses point clouds, publishes fused point cloud
+Subscribers:    /saved_pcs - most recent saved PointCloud2 message
+Publisher:      /fused_pc - concatenated PointCloud2 message
+*/
 {
     private:
     ros::Subscriber pcsub;
@@ -12,11 +16,20 @@ class FusePC
 
     public:
     FusePC(ros::NodeHandle *nh)
+    /*
+    constructor of FusePC class
+    initializes subscriber and publisher
+    */
     {
-        pcsub = nh->subscribe("saved_pcs",1000,&FusePC::fusion,this);
+        pcsub = nh->subscribe("/saved_pcs",1000,&FusePC::fusion,this);
         pcpub = nh->advertise<sensor_msgs::PointCloud2>("/fused_pc", 10, true); 
     }
     void fusion(const sensor_msgs::PointCloud2 PCmsg)
+    /*
+    subsciber callback function
+    fuses PointCloud2 messages published to saved_pcs topic
+    publishes fused PointCloud2 message to /fused_pc
+    */
     {
         pcl::concatenatePointCloud(pcfused, PCmsg, pcfused);
         pcpub.publish(pcfused);
