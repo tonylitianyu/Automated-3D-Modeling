4cd72ac318e63534352dfb64aba07d0deb09c924
Arun Kumar <arunkumar2021@u.northwestern.edu>
2020-11-19 16:22:17 -0600

    fuse_pc node fully functional

--- camera_reconstruct/nodes/fuse_pc.cpp
+++ camera_reconstruct/nodes/fuse_pc.cpp
@@ -1,20 +1,25 @@
 #include "ros/ros.h"
 #include "sensor_msgs/PointCloud2.h"
-#include "std_msgs/String.h"
+// #include "std_msgs/String.h"
+#include "pcl_conversions/pcl_conversions.h"
 
 class FusePC
 {
     private:
     ros::Subscriber pcsub;
+    ros::Publisher pcpub;
+    sensor_msgs::PointCloud2 pcfused;
 
     public:
     FusePC(ros::NodeHandle *nh)
     {
-        pcsub = nh->subscribe("saved_pcs",1000,&FusePC::mycallback,this);
+        pcsub = nh->subscribe("saved_pcs",1000,&FusePC::fusion,this);
+        pcpub = nh->advertise<sensor_msgs::PointCloud2>("/fused_pc", 10, true); 
     }
-    void mycallback(const sensor_msgs::PointCloud2 PCmsg)
+    void fusion(const sensor_msgs::PointCloud2 PCmsg)
     {
-        ROS_INFO_STREAM(PCmsg.header);
+        pcl::concatenatePointCloud(pcfused, PCmsg, pcfused);
+        pcpub.publish(pcfused);
     }
 };
 
